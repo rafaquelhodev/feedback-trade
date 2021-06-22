b717afeef40a816f0b730a101c3d9e0f163887c0
rafael.quelho <rafael.quelho@norus.com.br>
2021-06-22 20:35:13 -0300

    Dealing with exceptions with middleware

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,19 +3,13 @@ import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUserController {
     async handle(request: Request, response: Response) {
+        const { name, email, admin } = request.body;
 
-        try {
-            const { name, email, admin } = request.body;
+        const createUserService = new CreateUserService();
 
-            const createUserService = new CreateUserService();
+        const user = await createUserService.execute({ name, email, admin });
 
-            const user = await createUserService.execute({ name, email, admin });
-
-            return response.json(user);
-        } catch (error) {
-            return response.status(400).json({ error: error.message });
-        }
-
+        return response.json(user);
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors'
 
 import './database';
 import { router } from './routes';
@@ -9,6 +10,17 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({ error: err.message });
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening port ${PORT}`));
