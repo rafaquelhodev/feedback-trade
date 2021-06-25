b39e354e69dc3fb5ae4e83e5e6c4fac8382b39fc
rafael.quelho <rafael.quelho@norus.com.br>
2021-06-25 12:55:29 -0300

    auth middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+    const { user_id } = req;
 
-    if (admin) {
+    const usersRepository = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (user.admin) {
         return next();
     }
 
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateTagController } from './controllers/CreateTagController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { ComplimentController } from './controllers/ComplimentController';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
 
@@ -13,9 +14,8 @@ const authenticateUserController = new AuthenticateUserController();
 const complimentController = new ComplimentController();
 
 router.post("/users", createUserController.handle);
-router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/auth", authenticateUserController.handle);
-router.post("/auth", authenticateUserController.handle);
-router.post("/compliments", complimentController.handle);
+router.post("/compliments", ensureAuthenticated, complimentController.handle);
 
 export { router }
